0137dcc26b9136df534200dcf3d5dfe4de183a15
NickaNii <nicholasniels@gmail.com>
2025-05-12 10:11:38 +0200

    Created flask app

---
+++ Flask/app.py
@@ -0,0 +1,15 @@
+# imports
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# app routing and run
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def about():
+    return render_template('login.html')
+
+app.run(debug=True)
