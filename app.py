5573d491aa2d7a3e07c6c87004a887d5872681fb
NickaNii <nicholasniels@gmail.com>
2025-05-17 15:21:02 +0200

    updates

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # app routing and run
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('login.html')
 
 @app.route('/login')
 def about():
