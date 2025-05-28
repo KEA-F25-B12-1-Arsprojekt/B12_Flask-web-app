6b5b5ed670984b03a3e76014701748bf8d44bed5
Nicholas <74982293+NickaNii@users.noreply.github.com>
2025-05-28 17:57:19 +0200

    Added dynamic nav item

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # imports
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
