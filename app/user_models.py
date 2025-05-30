ded07164f9b9372a2f15c73cc807fd2c036841ed
NickaNii <nicholasniels@gmail.com>
2025-05-30 13:28:32 +0200

    readability

--- app/user_models.py
+++ app/user_models.py
@@ -4,8 +4,8 @@ from flask_login import UserMixin
 from app import db
 
 class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    firstname = db.Column(db.String(100), nullable=False)  # Add this if missing
-    lastname = db.Column(db.String(100), nullable=False)   # Add this if missing
-    password = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(100), unique = True, nullable = False)
+    firstname = db.Column(db.String(100), nullable = False)
+    lastname = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
