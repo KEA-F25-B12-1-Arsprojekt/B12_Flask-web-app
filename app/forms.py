6bfcc6adfbee32a1537a97fe9991e22b51b3ac20
NickaNii <nicholasniels@gmail.com>
2025-05-30 13:27:01 +0200

    Comments and readability

--- app/forms.py
+++ app/forms.py
@@ -5,10 +5,10 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username", validators = [DataRequired(), Length(min=4, max=20)])
-    password = PasswordField("Password", validators = [DataRequired(), Length(min=6, max=20)])
-    firstname = StringField("First name", validators= [DataRequired()])
-    lastname = StringField("Last name", validators=[DataRequired()])
+    username = StringField("Username", validators = [DataRequired(), Length(min = 4, max = 20)])
+    password = PasswordField("Password", validators = [DataRequired(), Length(min = 6, max = 20)])
+    firstname = StringField("First name", validators = [DataRequired()])
+    lastname = StringField("Last name", validators = [DataRequired()])
     submit = SubmitField("Register")
 
 class LoginForm(FlaskForm):
